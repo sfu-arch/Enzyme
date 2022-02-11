39d98139c0ed2782f396f7ea5b565cbe4f64909b
miladhakimi <miladhakimi33@gmail.com>
2022-02-11 09:20:06 -0800

    Add Tape cost
    Tape cost is added to the reverse mode operations who have direct or indirect edges to the forward pass

--- enzyme/Enzyme/CostAnalysis.cpp
+++ enzyme/Enzyme/CostAnalysis.cpp
@@ -24,12 +24,17 @@ bool InstruMemPass::runOnFunction(Function &f)
     for (auto &BB : f) {
         for (auto &I : BB) {
             auto &context = I.getContext();
-            if (BB.getName().contains("invert"))
+            if (I.getName().contains("'de"))
+                memOps[I.getName().str()] = std::make_pair(0, 0);
+
+            if (BB.getName().contains("invert") || I.getName().contains("'de")) {
                 I.setMetadata("mode", MDNode::get(context, MDString::get(context, "reverse")));
+                I.setMetadata("tapeCost", MDNode::get(context, MDString::get(context, "0")));
+            }
             else
                 I.setMetadata("mode", MDNode::get(context, MDString::get(context, "forward")));
-
-            I.setMetadata("cost", MDNode::get(context, MDString::get(context, "0")));
+            
+            I.setMetadata("calcCost", MDNode::get(context, MDString::get(context, "0")));
         }
     }
     F = &f;
@@ -42,12 +47,49 @@ void InstruMemPass::visitBinaryOperator(BinaryOperator &ins)
 {
     auto op1 = ins.getOperand(0);
     auto op2 = ins.getOperand(1);
-    uint32_t op1_cost = getCost(op1);
-    uint32_t op2_cost = getCost(op2);
-    uint32_t maxCost = std::max(op1_cost, op2_cost);
-    ins.setMetadata("cost", MDNode::get(ins.getContext(), MDString::get(ins.getContext(), std::to_string(maxCost+1))));
-    errs() << "BinaryOperator: " << maxCost + 1 << "\n";
+    uint32_t op1_calc_cost = getCalcCost(op1);
+    uint32_t op2_calc_cost = getCalcCost(op2);
+    uint32_t op1_tape_cost = 0, op2_tape_cost = 0;
+    
+    if (isReverseNode(&ins)) {
+        uint32_t tapeCost = 0;
+        op1_tape_cost = getTapeCost(op1);
+        op2_tape_cost = getTapeCost(op2);
+        errs() << "op1_tape_cost: " << op1_tape_cost << "\n";
+        errs() << "op2_tape_cost: " << op2_tape_cost << "\n";
+        tapeCost = op1_tape_cost + op2_tape_cost;
+        ins.setMetadata("tapeCost", MDNode::get(ins.getContext(), MDString::get(ins.getContext(), std::to_string(tapeCost))));
+    }
+    
+    uint32_t maxCost = std::max(op1_calc_cost, op2_calc_cost);
+    ins.setMetadata("calcCost", MDNode::get(ins.getContext(), MDString::get(ins.getContext(), std::to_string(maxCost+1))));
+}
 
+void InstruMemPass::visitStoreInst(StoreInst &ins) {
+    auto *op1 = ins.getOperand(0);
+    if (isReverseNode(op1)) {
+        memOps[ins.getOperand(1)->getName().str()].first = getCalcCost(ins.getOperand(0));
+        memOps[ins.getOperand(1)->getName().str()].second = getTapeCost(ins.getOperand(0));
+    }
+}
+void InstruMemPass::visitLoadInst(LoadInst &ins) {
+    if (!isReverseNode(&ins))
+        return;
+    auto *op1 = ins.getOperand(0);
+    if (memOps.count(op1->getName().str())) {
+        ins.setMetadata("tapeCost", MDNode::get(ins.getContext(), MDString::get(ins.getContext(), std::to_string(memOps[op1->getName().str()].second))));
+        ins.setMetadata("calcCost", MDNode::get(ins.getContext(), MDString::get(ins.getContext(), std::to_string(memOps[op1->getName().str()].first))));
+    }
+}
+
+bool InstruMemPass::isReverseNode(Value *V)
+{
+    if (!isa<Instruction>(*V))
+        return false;
+    Instruction &I = cast<Instruction>(*V);
+    auto *N = I.getMetadata("mode");
+    auto *S = dyn_cast<MDString>(N->getOperand(0));
+    return S->getString().str() == "reverse";
 }
 
 char InstruMemPass::ID = 0;
--- enzyme/Enzyme/CostAnalysis.h
+++ enzyme/Enzyme/CostAnalysis.h
@@ -9,6 +9,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include <map>
 
 
 using namespace llvm;
@@ -19,19 +20,35 @@ namespace instrumem
     struct InstruMemPass : public llvm::FunctionPass,
                            llvm::InstVisitor<InstruMemPass>
     {
-
+    
     private:
         const std::string pre = "__InstruMem_";
-
-        uint32_t getCost(Value *V) {
+        std::map<std::string, std::pair<uint32_t, uint32_t>> memOps;
+        uint32_t getCalcCost(Value *V) {
             if (!isa<Instruction>(*V))
                 return 0;
             Instruction &I = cast<Instruction>(*V);
-            auto *N = I.getMetadata("cost");
+            auto *N = I.getMetadata("calcCost");
             auto *S = dyn_cast<MDString>(N->getOperand(0));
             return stoi(S->getString().str());
         };
 
+        uint32_t getTapeCost(Value *V) {
+            if (!isa<Instruction>(*V))
+                return 0;
+            Instruction &I = cast<Instruction>(*V);
+            auto *N = I.getMetadata("mode");
+            auto S = dyn_cast<MDString>(N->getOperand(0));
+            auto mode = S->getString().str();
+            if (mode == "forward")
+                return 1;
+
+            N = I.getMetadata("tapeCost");
+            S = dyn_cast<MDString>(N->getOperand(0));
+            return stoi(S->getString().str());
+        }
+
+        bool isReverseNode(Value *V);
     public:
         static char ID;
         llvm::Function *F = nullptr;
@@ -46,6 +63,8 @@ namespace instrumem
         bool runOnFunction(llvm::Function &f) override;
 
         void visitBinaryOperator(BinaryOperator &ins);
+        void visitStoreInst(StoreInst &ins);
+        void visitLoadInst(LoadInst &ins);
 
     };
 

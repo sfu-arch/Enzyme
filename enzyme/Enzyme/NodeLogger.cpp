9f58f1b6920a63c8c7367b3955249cf82b219296
miladhakimi <miladhakimi33@gmail.com>
2022-04-06 15:49:45 -0700

    Bug fix in load operations

--- enzyme/Enzyme/NodeLogger.cpp
+++ enzyme/Enzyme/NodeLogger.cpp
@@ -7,7 +7,9 @@ using namespace instrumem;
 cl::opt<bool> ignore_reverse("ignore-reverse", cl::init(false),
                                  cl::Hidden,
                                  cl::desc("Doens't include reverse nodes in the analysis"));
-
+extern "C" {
+    int num_live_values = 0;
+}
 NodeLogger::NodeLogger() : FunctionPass(ID) {} // NodeLogger
 
 void PrintFunctionArgs(Function &f) {
@@ -15,7 +17,9 @@ void PrintFunctionArgs(Function &f) {
 
     for (int i=0; i<f.arg_size(); i++) {
         ConstantInt *id = ConstantInt::get(Type::getInt32Ty(first_inst->getContext()), i+1);
-        CallPrintf(first_inst, "Node: %u, Arg\n", {id}, "0");
+        std::string write_format = "F_Node: " + f.getArg(i)->getNameOrAsOperand() + ", Parent: %x, load\n";
+
+        CallPrintf(first_inst, &write_format[0], {}, write_format);
     }
 
 }
@@ -37,7 +41,7 @@ bool NodeLogger::runOnFunction(Function &f) {
                 visit(cast<Instruction>(i.first));
 
     PrintFunctionArgs(f);
-    
+    errs() << "Live value count: " << num_live_values << "\n";
     return true;
 }
 
@@ -48,17 +52,16 @@ std::string ModePrefix(Instruction *i) {
 }
 
 void NodeLogger::visitLoadInst(LoadInst &inst) {
-    std::string write_format = ModePrefix(&inst) + "Node: %u, Parent: %x, load\n";
-
-    auto *load_id = ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[&inst]);
-    CallPrintf(&inst, &write_format[0], {load_id, inst.getPointerOperand()}, "load_1");
+    std::string write_format = ModePrefix(&inst) + "Node: " + inst.getNameOrAsOperand() + ", Parent: %x, load\n";
+    // auto *load_id = ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[&inst]);
+    CallPrintf(&inst, &write_format[0], {inst.getPointerOperand()}, write_format);
 }
 
 
 void NodeLogger::visitStoreInst(StoreInst &inst) {
-    std::string write_format = ModePrefix(&inst) + "Node: %x, Parent: %d, store\n";
+    std::string write_format = ModePrefix(&inst) + "Node: %x, Parent: " + inst.getValueOperand()->getNameOrAsOperand()+ ", store\n";
     auto *value_id = ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[inst.getValueOperand()]);
-    CallPrintf(&inst, &write_format[0], {inst.getPointerOperand(), value_id,}, "store_1");
+    CallPrintf(&inst, &write_format[0], {inst.getPointerOperand()}, write_format);
 }
 
 std::string GenerateWriteFormat(Instruction &inst, std::map<Value*, unsigned> &node_ids) {
@@ -66,7 +69,6 @@ std::string GenerateWriteFormat(Instruction &inst, std::map<Value*, unsigned> &n
     if (isa<Instruction>(inst)) {
         if (isa<BinaryOperator>(inst)) {
             write_format += "arithmetic\n";
-            errs() << node_ids[&inst] << ": " << inst << "\n";
         } else{
             write_format += std::string(inst.getOpcodeName())+  "\n";
         }
@@ -80,21 +82,21 @@ std::string GenerateWriteFormat(Instruction &inst, std::map<Value*, unsigned> &n
 void NodeLogger::visitInstruction(Instruction &inst) {
     if (isa<ReturnInst>(inst) || isa<BranchInst>(inst))
         return;
-    std::string write_format = "Node: %u, ";
+    std::string write_format = "Node: " + inst.getNameOrAsOperand() + ", ";
     ConstantInt *id = ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[&inst]);
-    std::vector<Value*> args={id};
+    std::vector<Value*> args={};
 
     for (int i=0; i < inst.getNumOperands(); i++) {
         Value *operand = inst.getOperand(i);
         if (!isa<Constant>(operand)) {
-            args.push_back(ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[operand]));
-            write_format += "Parent: %u, ";
+            // args.push_back(ConstantInt::get(Type::getInt32Ty(inst.getContext()), node_ids[operand]));
+            write_format += "Parent: " + operand->getNameOrAsOperand() + ", ";
         }
     }
 
     write_format += GenerateWriteFormat(inst, node_ids);
     write_format =  ModePrefix(&inst) + write_format;
-    CallPrintf(&inst, &write_format[0], args, std::to_string(args.size()) + write_format);
+    CallPrintf(&inst, &write_format[0], {}, write_format);
 }
 
 char NodeLogger::ID = 0;

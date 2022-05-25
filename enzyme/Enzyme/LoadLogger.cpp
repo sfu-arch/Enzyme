9998cdb1d47f56f65b82fe5536e318041b3a7ea6
miladhakimi <miladhakimi33@gmail.com>
2022-05-25 09:50:04 -0700

    Log load store addresses

---
+++ enzyme/Enzyme/LoadLogger.cpp
@@ -0,0 +1,53 @@
+//===- InstruMem.cpp ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements an LLVM memory operation instrumentation pass.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LoadLogger.h"
+#include "llvm/IR/IRBuilder.h"
+#include "Utils.h"
+
+using namespace instrumem;
+
+LoadLoggerPass::LoadLoggerPass() : FunctionPass(ID) {} // LoadLoggerPass
+
+bool LoadLoggerPass::runOnFunction(Function &f)
+{
+    errs() << "LoadLogger ...\n";
+    std::vector<Instruction *> ops;
+
+    for (auto &BB : f)
+        for (auto &I : BB)
+            if (isa<LoadInst>(I) || isa<StoreInst>(I))
+                ops.push_back(&I);
+
+    for (auto op: ops)
+        visit(*op);
+
+    return false;
+}
+
+
+void LoadLoggerPass::visitInstruction(Instruction &inst) {
+    errs() << " Sssssssssss " << inst << "\n";
+    // This function calls the printf after the given instruction.
+    // HOW TO USE:
+    //     std::vector<Value*> args = {arg0, arg1, arg2, ...};
+    //     CallPrintf(call_before, format, args);
+    // 
+    auto pointer_operand = isa<LoadInst>(inst) ? inst.getOperand(0) : inst.getOperand(1);
+    std::string format = pointer_operand->getNameOrAsOperand() + ", %u\n";
+    CallPrintf(inst.getNextNode(), &format[0], {pointer_operand}, format);
+}
+
+char LoadLoggerPass::ID = 1;
+static RegisterPass<LoadLoggerPass> X("loadlpass", "LoadLogger Pass");
+
---
+++ enzyme/Enzyme/LoadLogger.h
@@ -0,0 +1,36 @@
+
+#ifndef LOAD_LOGGER_H
+#define LOAD_LOGGER_H
+
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+#include <map>
+
+using namespace llvm;
+
+namespace instrumem
+{
+
+    struct LoadLoggerPass : public llvm::FunctionPass,
+                           llvm::InstVisitor<LoadLoggerPass>
+    {
+    
+    public:
+        static char ID;
+        llvm::Function *F = nullptr;
+
+        LoadLoggerPass();
+
+        bool runOnFunction(llvm::Function &f) override;
+        void visitInstruction(llvm::Instruction &ins);
+
+    };
+
+} // namespace instrumem
+
+#endif
